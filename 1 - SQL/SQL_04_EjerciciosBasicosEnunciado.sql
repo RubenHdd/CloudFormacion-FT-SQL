9b99881b29fc3ac44f500bd484a31bff4ddf984b
RubenHdd <rubenhdr@gmail.com>
2024-03-15 23:31:04 +0100

    150324 - Video nuevo

---
+++ 1 - SQL/SQL_04_EjerciciosBasicosEnunciado.sql
@@ -0,0 +1,40 @@
+-- Tabla: sakila.actor
+-- Pregunta: Actores que se llamen 'Scarlett'
+
+-- Tabla: sakila.actor
+-- Pregunta: Actores que se apelliden 'Johansson'
+
+-- Tabla: sakila.actor
+-- Pregunta: ¿Cuántos apellidos de actor (last_name) diferentes hay?
+
+-- Tabla: sakila.film
+-- Pregunta: Número de películas por año
+
+-- Tabla: sakila.film
+-- Pregunta: Duración máxima y mínima de las peliculas registradas
+
+-- Tabla: sakila.film
+-- Pregunta: Películas ordenadas por duración descendente
+
+-- Tabla: sakila.film
+-- Pregunta: Películas con duración entre 80 y 120 minutos
+
+-- Tabla: sakila.actor
+-- Pregunta: Actores que tengan una x en el nombre o en el apellido
+
+-- Tabla: sakila.address
+-- Pregunta: Direcciones de california que tengan ‘274’ en el número de teléfono
+
+-- Tabla: sakila.film
+-- Pregunta: Películas ‘Épicas’ (Epic) o ‘Brillantes’ (brilliant) que duren más de 180 minutos
+
+-- Tabla: sakila.film
+-- Pregunta: Películas que duren entre 100 y 120 minutos o entre 50 y 70 minutos
+
+-- Tabla: sakila.film
+-- Pregunta: Películas que cuesten 0.99, 2.99 y tengan un rating ‘g’ o ‘r’ y que hablen de cocodrilos (cocodrile)
+
+-- Tabla: sakila.address
+-- Pregunta: Direcciones de ontario o de punjab o que su código postal acabe en 5 o que su teléfono acabe en 5
+
+
