80864b5a0debbfe3aa43a7e88605dc546c361cae
RubenHdd <rubenhdr@gmail.com>
2024-03-30 13:18:31 +0100

    Create SQL_07_EjerciciosDDLEnunciado.sql

---
+++ 1 - SQL/SQL_07_EjerciciosDDLEnunciado.sql
@@ -0,0 +1,37 @@
+-- Ejercicio 1: Tabla de Empleados de una Librería
+-- Esta tabla almacenará información sobre los empleados de una librería.
+-- Cada empleado tendrá:
+-- 		identificador único
+--		nombre_empresa
+--		apellido 
+--		cargo
+--		salario
+--		fecha de contratación
+-- Además, asegúrate de incluir las siguientes restricciones:
+-- 		El salario no puede ser negativo.
+-- 		Todos los campos excepto el salario deben ser obligatorios (no nulos).
+--		El ID deberá rellenarse automáticamente
+
+
+
+-- Ejercicio 2: Tabla de Proveedores de una Librería
+-- Esta tabla registrará información sobre los proveedores que suministran productos a la librería.
+-- Cada proveedor tendrá: 
+-- 		identificador único
+--		nombre de la empresa
+--		persona de contacto
+--		número de teléfono
+--		correo electrónico
+-- Además, asegúrate de incluir las siguientes restricciones:
+-- 		El correo electrónico debe seguir un formato válido.
+--		El nombre de la empresa debe ser obligatorio (no nulo).
+--		El ID deberá rellenarse automáticamente
+
+
+
+-- Ejercicio 3: Añade una nueva columna a la tabla de libros existente.
+-- PISTA: Puedes usar la interfaz visual.
+-- Añade las columnas precio y stock a dicha tabla.
+
+-- BONUS: ¿Cómo haría si quisiese añadir una restricción para que precio y stock fuesen mayor o igual a 0?
+
